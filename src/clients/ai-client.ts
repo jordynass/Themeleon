588441a15ac33751890504f45f2ca2eaccef0a76
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2024-11-01 15:35:29 -0400

    Add SVG request to prompt.

--- src/clients/ai-client.ts
+++ src/clients/ai-client.ts
@@ -32,17 +32,20 @@ The visual theme has the following parameters
 on that counties on that flag. If the theme is water, it could be colors typical of water like shades of blue and green.
 If the theme is joy, the colors should be bright. If the theme is the 1960s, it could be common tie dye color.
 
-The colors should be formatted as comma-separate rgb triples from 0 to 255.
+The colors should be formatted as comma-separate rgb triples from 0 to 255. They should be ordered in the response
+in decreasing order of relevance.
 </Colors>
 
 <Icons>
-Names or brief descriptions of a few simple icons (3 to 7) associated theme of ${themePrompt}. They should be objects that
+Come up with a few simple icons (3 to 7) associated theme of ${themePrompt}. They should be simple objects that
 are simple enough that they can be recognizably drawn as an SVGs without much trouble. For instance,
 
 Ireland: Clover, Irish flag, Cabbage, Mountain, Emerald, Harp
 Water: Wave, Fish, Pool, Swimsuit, Bucket
 Joy: Smiley face, Thumbs up, Love heart
 1960s: Peace sign, Yin Yang, Tie dye t-shirt, Incense, Guitar
+
+Design a small (width and height between 20 and 50) SVG for each of these icons.
 </Icons>
 </Parameters>
 
@@ -52,11 +55,9 @@ It should be an XML object in the following format:
 <Color>0,140,69</Color>
 <Color>244,245,240</Color>
 <Color>205,33,42</Color>
-<Icon>Pizza slices</Icon>
-<Icon>Coliseum</Icon>
-<Icon>Boot</Icon>
-<Icon>Pasta</Icon>
-<Icon>Leaning Tower of Pisa</Icon>
-<Icon>Gondola boat</Icon>
+<Icon name="Boot"><svg height="30" width="50"...</svg></Icon>
+<Icon name="Pizza"><svg height="25" width="25"...</svg></Icon>
+<Icon name="Pasta"><svg height="40" width="40"...</svg></Icon>
+<Icon name="Gondola Boat"><svg height="30" width="45"...</svg></Icon>
 `;
 }
